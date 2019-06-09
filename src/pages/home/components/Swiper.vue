13d74c55775060c86551ed1a92ad6adbf6d9a118
FindIV <2897055662@qq.com>
2019-06-09 22:47:02 +0800

    banner finish

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -38,7 +38,8 @@ export default {
     overflow: hidden
     width: 100%
     height: 0
-    padding-bottom: 45.75%
+    padding-bottom: 31.25%
+    background: #eee
     .swiper-img
       width: 100%
 </style>
